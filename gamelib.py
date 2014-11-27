8d1972b6ed80e3eb5c6d3d48c2302ccbbc9c897b
Eiqueson <eiqueson@gmail.com>
2014-11-27 13:45:58 +0700

    Blank Window

--- gamelib.py
+++ gamelib.py
@@ -3,17 +3,30 @@ from pygame.locals import *
 
 class SimpleGame(object):
 
-	def __init__(self):
-		pass
+	def __init__(self, title, window_size=(480,640), fps=60):
+		self.title = title
+		self.window_size = window_size
+		self.fps = fps
 
 	def run(self):
-		pass
+		self.init()
+		while True:
+			self.update()
+			self.render()
+			self.clock.tick(self.fps)
 
 	def init(self):
-		pass
+		self.__game_init()
 
 	def update(self):
 		pass
 
 	def render(self):
 		pass
+
+	def __game_init(self):
+		pygame.init()
+		self.clock = pygame.time.Clock()
+		self.display = pygame.display.set_mode(self.window_size)
+		pygame.display.set_caption(self.title)
+		self.font = pygame.font.SysFont("monospace", 16)
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pygame
 from pygame.locals import *
 from gamelib import SimpleGame
+
+class MeteorGame(SimpleGame):
+	
+	def init(self):
+		super(MeteorGame, self).init()
+
+def main():
+	game = MeteorGame('Meteor Breaker')
+	game.run()
+
+if __name__ == '__main__':
+	main()
